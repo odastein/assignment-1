e9ea34fbb4643f454a73e2ffc682ba8618a56239
odast <oksteins@stud.ntnu.no>
2023-03-02 17:12:53 +0100

    Made main and structs

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"assignment-1/handlers"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Println("$PORT has not been set. Default port is: " + port)
+	}
+
+	http.HandleFunc("/", handlers.UniInfoHandler)
+	log.Println("Running on port: " + port)
+	log.Fatal(http.ListenAndServe(": "+port, nil))
+}
---
+++ handlers/structs.go
@@ -0,0 +1,10 @@
+package handlers
+
+type UniInfo struct {
+	Name      string            `json:"name"`
+	Country   string            `json:"country"`
+	Isocode   string            `json:"isocode"`
+	Webpages  []string          `json:"webpages"`
+	Languages map[string]string `json:"languages"`
+	Map       string            `json:"map"`
+}
