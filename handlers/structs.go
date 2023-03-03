edd7bcbc2d7c23dfbea56af27c3b9885d29a3630
odast <oksteins@stud.ntnu.no>
2023-03-03 12:34:17 +0100

    Made a struct for university

--- handlers/structs.go
+++ handlers/structs.go
@@ -8,3 +8,12 @@ type UniInfo struct {
 	Languages map[string]string `json:"languages"`
 	Map       string            `json:"map"`
 }
+
+type University struct {
+	AlphaTwoCode  string   `json:"alpha_two_code"`
+	WebPages      []string `json:"web_pages"`
+	StateProvince string   `json:"state-province"`
+	Name          string   `json:"name"`
+	Domains       []string `json:"domains"`
+	Country       string   `json:"country"`
+}
