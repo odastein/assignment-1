31f1e9f71fb7c6791be90d3966b395f151e8c73e
odast <oksteins@stud.ntnu.no>
2023-03-05 23:03:00 +0100

    Added name comverter for country names

--- handlers/constants.go
+++ handlers/constants.go
@@ -6,10 +6,33 @@ const NeighbourUniPath = rootPath + "neighbourunis/"
 const DiagPath = rootPath + "diag/"
 
 const UniversityURL = "http://universities.hipolabs.com"
-const SearchURL = "/search?name="
+const SearchNameURL = "/search?name="
+const SearchCountryURL = "&search?country="
 
 const RestCountriesPath = "https://restcountries.com"
 const RestCountriesNamePath = RestCountriesPath + "/v3.1/name/"
 const RestCountriesAlphaPath = RestCountriesPath + "/v3.1/alpha/"
 const RestCountriesTextPath = "?fullText=true"
 const RestCountriesFieldsPath = "?fields=name,maps,languages,borders"
+
+var countryNames = map[string]string{
+	"Bolivia":                "Bolivia, Plurinational State of",
+	"Syria":                  "Syrian Arab Republic",
+	"Palestine":              "Palestine, State of",
+	"Macau":                  "Macao",
+	"Ivory Coast":            "Côte d'Ivoire",
+	"Moldova":                "Moldova, Republic of",
+	"Republic of the Congo":  "Congo",
+	"Czechia":                "Czech Republic",
+	"Vietnam":                "Viet Nam",
+	"DR Congo":               "Congo, the Democratic Republic of the",
+	"South Korea":            "Korea, Republic of",
+	"North Korea":            "Korea, Democratic People's Republic of",
+	"Russia":                 "Russian Federation",
+	"Laos":                   "Lao People's Democratic Republic",
+	"Eswatini":               "Swaziland",
+	"Venezuela":              "Venezuela, Bolivarian Republic of",
+	"Tanzania":               "Tanzania, United Republic of",
+	"British Virgin Islands": "Virgin Islands, British",
+	"Vatican City":           "Holy See (Vatican City State)",
+	"Brunei":                 "Brunei Darussalam"}
