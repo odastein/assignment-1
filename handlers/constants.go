b9608b78f0816ed96d1c3c59256be34e45f953a6
odast <oksteins@stud.ntnu.no>
2023-03-05 19:32:22 +0100

    Added more constants

--- handlers/constants.go
+++ handlers/constants.go
@@ -9,3 +9,7 @@ const UniversityURL = "http://universities.hipolabs.com"
 const SearchURL = "/search?name="
 
 const RestCountriesPath = "https://restcountries.com"
+const RestCountriesNamePath = RestCountriesPath + "/v3.1/name/"
+const RestCountriesAlphaPath = RestCountriesPath + "/v3.1/alpha/"
+const RestCountriesTextPath = "?fullText=true"
+const RestCountriesFieldsPath = "?fields=name,maps,languages,borders"
